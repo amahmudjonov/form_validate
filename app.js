ea9e65dd4e6c87b8d0cc95919f034008127d51d7
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-05-14 12:20:16 +0200

    add all the variables/selectors to app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+// Selectors
+const form = document.querySelector(".signup-form");
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("email");
+const password = document.getElementById("password_el");
+const eyeClick = document.querySelector("[data-password]");
+const button = document.querySelector(".submit-btn");

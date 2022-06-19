2959393915569777665b39b99e4fc3023ec657dd
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-19 06:27:13 +0200

    delete uneccessary comment

--- app.js
+++ app.js
@@ -31,27 +31,25 @@ function checksInputs() {
   const emailValue = email.value.trim();
   const passwordValue = password.value.trim();
   const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  // First Name Statement
+
   if (firstNameValue === "") {
     firstName.style.border = "2px solid red";
   } else {
     firstName.style.border = "2px solid #eeeeee";
   }
-  // Last Name Statement
+
   if (lastNameValue === "") {
     lastName.style.border = "2px solid red";
   } else {
     lastName.style.border = "2px solid #eeeeee";
   }
 
-  // Email Statement
   if (!emailValue === "" || emailValue.match(mailformat)) {
     email.style.border = "2px solid #eeeeee";
   } else {
     email.style.border = "2px solid red";
   }
 
-  // Password Statement
   if (passwordValue === "") {
     password.style.border = "2px solid red";
   } else {
